867713a0f8aed1135d6b7e88cf84c167dfb36b71
webdevsk <72684302+webdevsk@users.noreply.github.com>
2025-01-07 16:38:03 +0600

    Moved homepage into subdirectory

--- client/app/(home)/layout.tsx
+++ client/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Onest } from "next/font/google";
 import "./globals.css";
-import { Footer } from "../components/footer"
+import { Footer } from "../../components/footer"
 
 const onest = Onest({
   subsets: ["latin"],
