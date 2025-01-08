a8a712c9ea9f978243e3300d130b8321098648f5
webdevsk <72684302+webdevsk@users.noreply.github.com>
2025-01-08 20:04:42 +0600

    Done with animating header

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,27 +1,37 @@
 "use client"
 
-import { useState } from "react"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
+import useDetectScroll from "@smakss/react-scroll-direction"
 import { DialogContent, DialogRoot, DialogTrigger } from "./dialog-menu"
 import { AnimatedButton } from "./ui/animated-button"
 import { BurgerMenuBtn } from "./ui/burger-menu-btn"
 
 export function Header() {
-  const [open, setOpen] = useState(false)
+  const { scrollDir, scrollPosition } = useDetectScroll()
+  const translate = scrollPosition.top === 0 ? 0 : scrollDir === "up" ? 0 : 1
   return (
-    <header className="container fixed top-0 z-40 flex h-[100px] items-center gap-8">
-      <Logo className="max-w-16" />
-      <AnimatedButton variant="outline" className="ms-auto flex h-10 items-center px-6 py-0 text-base font-medium">
-        <Link href="/admin">Admin Panel</Link>
-      </AnimatedButton>
-      <DialogRoot>
-        <DialogTrigger asChild>
-          <BurgerMenuBtn />
-        </DialogTrigger>
-        <DialogContent title="Main Menu">Hello</DialogContent>
-      </DialogRoot>
-    </header>
+    <>
+      <header
+        style={{ ["--translate" as string]: translate }}
+        className="container fixed top-0 z-40 flex h-[100px] translate-y-[calc(var(--translate)_*_-100%)] items-center gap-8 transition-transform duration-700">
+        <Logo className="max-w-16" />
+        <AnimatedButton variant="outline" className="ms-auto flex h-10 items-center px-6 py-0 text-base font-medium">
+          <Link href="/admin">Admin Panel</Link>
+        </AnimatedButton>
+        <DialogRoot>
+          <DialogTrigger asChild>
+            <BurgerMenuBtn />
+          </DialogTrigger>
+          <DialogContent title="Main Menu">Hello</DialogContent>
+        </DialogRoot>
+      </header>
+      <div
+        style={{ ["--translate" as string]: translate }}
+        className="pointer-events-none fixed top-0 z-30 h-[100px] w-full translate-y-[calc(var(--translate)_*_-100%)] transition-transform delay-100 duration-300">
+        <div className="h-full w-full backdrop-blur-md" />
+      </div>
+    </>
   )
 }
 
